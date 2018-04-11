7304ee2ff47f49fd6a7a17d975b20a2415204732
Imran Gardezi <igardezi1993@gmail.com>
2018-04-11 16:29:58 +0100

    Added Room

--- app/src/main/java/gardezi/io/amiibofinder/model/Amiibo.kt
+++ app/src/main/java/gardezi/io/amiibofinder/model/Amiibo.kt
@@ -1,11 +1,17 @@
 package gardezi.io.amiibofinder.model
 
-data class Amiibo(val amiiboSeries: String,
-                  val character: String,
-                  val gameSeries: String,
-                  val head: String,
-                  val image: String,
-                  val name: String,
-                  val release: Map<String, String>,
-                  val tail: String,
-                  val type: String)
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+data class Amiibo(
+        @PrimaryKey val uid: Int,
+        @ColumnInfo(name = "amiibo_series") val amiiboSeries: String,
+        @ColumnInfo(name = "character") val character: String,
+        @ColumnInfo(name = "game_series") val gameSeries: String,
+        @ColumnInfo(name = "head") val head: String,
+        @ColumnInfo(name = "image") val image: String,
+        @ColumnInfo(name = "name") val name: String,
+        @ColumnInfo(name = "tail") val tail: String,
+        @ColumnInfo(name = "type") val type: String)
